a2ca207cfd6f5404b3ef3e4ae49a6f5e25c598da
Damien Lecointre <damien.lecointre75@gmail.com>
2025-06-04 18:32:10 +0200

    frontend file added to github

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,9 @@
-import styles from '../styles/Home.module.css';
+import React from "react";
 
 function Home() {
   return (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+      <h1>Home</h1>
     </div>
   );
 }
