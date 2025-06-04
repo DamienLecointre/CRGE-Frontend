bd493883075b8d5bf8f76f4bdfba942232447174
Damien Lecointre <damien.lecointre75@gmail.com>
2025-06-04 18:59:39 +0200

    img import + nav folder & UiKit folder created

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>
+        Home
+        <FontAwesomeIcon icon={faArrowRight} />
+      </h1>
     </div>
   );
 }
