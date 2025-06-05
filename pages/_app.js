b687c1ae1f0ca39988dee6035c3b531c710fd2f6
Damien Lecointre <damien.lecointre75@gmail.com>
2025-06-05 10:05:01 +0200

    SEO added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,42 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>
+          CRGE - Conseil et Ressource pour les Groupements d'Employeurs
+        </title>
+        <meta
+          name="description"
+          content="CRGE est le réseau national qui réunit une centaine de Groupements d'Employeurs représentant plus de 5 000 salariés."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        {/* <link rel="canonical" href="https://www.tonsite.com/ton-url/" /> */}
+        <link rel="icon" type="image/svg+xml" href="/logo_icons/favicon.svg" />
+        {/* Open Graph
+        <meta property="og:title" content="Titre pour les réseaux sociaux" />
+        <meta
+          property="og:description"
+          content="Description pour les réseaux sociaux"
+        />
+        <meta
+          property="og:image"
+          content="https://www.tonsite.com/images/partage.jpg"
+        />
+        <meta property="og:url" content="https://www.tonsite.com/ton-url/" />
+        <meta property="og:type" content="website" />
+
+        Twitter Card
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Titre Twitter" />
+        <meta name="twitter:description" content="Description Twitter" />
+        <meta
+          name="twitter:image"
+          content="https://www.tonsite.com/images/twitter.jpg"
+        /> */}
       </Head>
       <Component {...pageProps} />
     </>
